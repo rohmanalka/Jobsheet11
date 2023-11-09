bfe18d58b122c0952888b4fb1fbea1ac76612704
rohmanalka <rohmanalka06@gmail.com>
2023-11-09 10:15:59 +0700

    first

---
+++ Tugas2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TugasK2 {
+    public static void main(String[] args) {
+        Scanner k2 = new Scanner(System.in);
+        System.out.print("Masukkan nilai N (minimal 5): ");
+        int N = k2.nextInt();
+
+        if (N < 5) {
+            System.out.println("Nilai N minimal harus 5.");
+            return;
+        }
+
+        for (int i = N; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
